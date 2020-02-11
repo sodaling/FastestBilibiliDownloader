6a991b295e27753dd9cb2d1fc173f443a4b4e664
sodaling <502464760@qq.com>
2020-02-11 19:52:34 +0800

    add the scheduler feature

---
+++ scheduler/scheduler.go
@@ -0,0 +1,47 @@
+package scheduler
+
+import "simple-golang-crawler/engine"
+
+type ConcurrentScheduler struct {
+	RequestsChan chan engine.Request
+	WorkerChan   chan chan engine.Request
+}
+
+func (s *ConcurrentScheduler) Run() {
+	s.WorkerChan = make(chan chan engine.Request)
+	s.RequestsChan = make(chan engine.Request)
+	go func() {
+		var workerQ []chan engine.Request
+		var requestQ []engine.Request
+
+		for {
+			var readyWorker chan engine.Request
+			var readyRequest engine.Request
+			if len(workerQ) > 0 && len(requestQ) > 0 {
+				readyWorker = workerQ[0]
+				readyRequest = requestQ[0]
+			}
+			select {
+			case readyRequest = <-s.RequestsChan:
+				requestQ = append(requestQ, readyRequest)
+			case readyWorker = <-s.WorkerChan:
+				workerQ = append(workerQ, readyWorker)
+			case readyWorker <- readyRequest:
+				requestQ = requestQ[1:]
+				workerQ = workerQ[1:]
+			}
+		}
+	}()
+}
+
+func (s *ConcurrentScheduler) GetWorkerChan() chan engine.Request {
+	return make(chan engine.Request)
+}
+
+func (s *ConcurrentScheduler) Submit(req engine.Request) {
+	s.RequestsChan <- req
+}
+
+func (s *ConcurrentScheduler) Ready(worker chan engine.Request) {
+	s.WorkerChan <- worker
+}
