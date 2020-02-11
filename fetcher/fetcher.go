84970cb13a84d3f5afd07501cac44d34ad0e2fdf
sodaling <502464760@qq.com>
2020-02-11 20:52:23 +0800

    add the fetcher feature

--- fetcher/fetcher.go
+++ fetcher/fetcher.go
@@ -1,5 +1,52 @@
 package fetcher
 
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+var rateLimiter = time.Tick(1 * time.Microsecond)
+
 func Fetch(url string) ([]byte, error) {
-	return nil, nil
+	<-rateLimiter
+	client := http.DefaultClient
+	request, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	request.Header.Add("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0")
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("wrong status code: %d", resp.StatusCode)
+	}
+
+	bodyReader := bufio.NewReader(resp.Body)
+
+	e := determineEncoding(bodyReader)
+	utf8Reader := transform.NewReader(bodyReader, e.NewDecoder())
+	defer resp.Body.Close() //注意放置的位置,否则可能出现invalid memory
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func determineEncoding(reader *bufio.Reader) encoding.Encoding {
+	bytes, err := reader.Peek(1024)
+	if err != nil {
+		log.Printf("Fetcher error: %v", err)
+		return unicode.UTF8
+	}
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module simple-golang-crawler
 
 go 1.12
+
+require (
+	golang.org/x/crypto v0.0.0-20200210222208-86ce3cb69678 // indirect
+	golang.org/x/mod v0.2.0 // indirect
+	golang.org/x/net v0.0.0-20200202094626-16171245cfb2 // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
+	golang.org/x/sys v0.0.0-20200202164722-d101bd2416d5 // indirect
+	golang.org/x/text v0.3.2 // indirect
+	golang.org/x/tools v0.0.0-20200211045251-2de505fc5306 // indirect
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543 // indirect
+)
