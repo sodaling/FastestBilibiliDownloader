9390cbe6a74bbbd21ceec1cd1f54b99b28ea9039
落心 <luoxin.ttt@gmail.com>
2021-06-01 10:46:55 +0800

    支持cli参数

--- cmd/start-concurrent-engine.go
+++ cmd/start-concurrent-engine.go
@@ -4,31 +4,37 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
+
 	"simple-golang-crawler/engine"
 	"simple-golang-crawler/parser"
 	"simple-golang-crawler/persist"
 	"simple-golang-crawler/scheduler"
-	"sync"
+
+	"github.com/alexflint/go-arg"
 )
 
-func main() {
-	itemProcessFun := persist.GetItemProcessFun()
-	var err error
-	var wg sync.WaitGroup
-	wg.Add(1)
-	itemChan, err := itemProcessFun(&wg)
-	if err != nil {
-		panic(err)
-	}
+var cmdArgs struct {
+	IdType string `arg:"-t,--type" help:"id type,support:\n\taid:\t\t下载单个视频\n\tupid\t\t下载指定up主的视频"`
+	Id     int64  `arg:"-i,--id" help:"视频或up主的id"`
+}
 
+func main() {
+	var err error
 	var idType string
 	var id int64
-	var req *engine.Request
-	fmt.Println("Please enter your id type(`aid` or `upid`)")
-	fmt.Scan(&idType)
-	fmt.Println("Please enter your id")
-	fmt.Scan(&id)
+	arg.MustParse(&cmdArgs)
+	if cmdArgs.IdType == "" {
+		fmt.Println("Please enter your id type(`aid` or `upid`)")
+		fmt.Scan(&idType)
+		fmt.Println("Please enter your id")
+		fmt.Scan(&id)
+	} else {
+		idType = cmdArgs.IdType
+		id = cmdArgs.Id
+	}
 
+	var req *engine.Request
 	if idType == "aid" {
 		req = parser.GetRequestByAid(id)
 	} else if idType == "upid" {
@@ -36,6 +42,14 @@ func main() {
 	} else {
 		log.Fatalln("Wrong type you enter")
 		os.Exit(1)
+	}
+
+	itemProcessFun := persist.GetItemProcessFun()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	itemChan, err := itemProcessFun(&wg)
+	if err != nil {
+		panic(err)
 	}
 
 	queueScheduler := scheduler.NewConcurrentScheduler()
